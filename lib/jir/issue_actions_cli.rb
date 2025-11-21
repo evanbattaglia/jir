816d09359286112bcf4659a52651be5b5a5b3e40
Evan Battaglia <ebattaglia@instructure.com>
2025-11-21 16:12:56 +0000

    wip untested -- claude -- jir transition names

--- lib/jir/issue_actions_cli.rb
+++ lib/jir/issue_actions_cli.rb
@@ -10,15 +10,51 @@ module Jir
 
     def transition
       transitions = JSON.parse(jira("issue/#{ticket_key}/transitions", shell_method: :backtick_or_die))
-      transitions = transitions["transitions"].map{|t| {name: t["to"]["name"], id: t["id"]}}
-      matching = transitions.select { |t| t[:name] == args.state_name }
-      case matching.length
+      transitions = transitions["transitions"].map{|t| {
+        to_name: t["to"]["name"],
+        transition_name: t["name"],
+        id: t["id"]
+      }}
+
+      # First try to match by destination state name
+      matching_by_state = transitions.select { |t| t[:to_name] == args.state_name }
+
+      # If no match by state name, try to match by transition name
+      if matching_by_state.empty?
+        matching_by_transition = transitions.select { |t| t[:transition_name] == args.state_name }
+        if matching_by_transition.length == 1
+          jira "issue/#{ticket_key}/transitions", "transition[id]=%s", [matching_by_transition.first[:id]], method: :post
+          return
+        elsif matching_by_transition.empty?
+          # Try numeric ID match as fallback
+          matching_by_id = transitions.select { |t| t[:id] == args.state_name }
+          if matching_by_id.length == 1
+            jira "issue/#{ticket_key}/transitions", "transition[id]=%s", [matching_by_id.first[:id]], method: :post
+            return
+          end
+        end
+      end
+
+      case matching_by_state.length
       when 0
-        raise "No matching transition to #{args.state_name.inspect} for this ticket. Possible end states: #{transitions.map { |t| t[:name] }}"
+        available_states = transitions.map { |t| t[:to_name] }.uniq
+        available_transitions = transitions.map { |t| "#{t[:transition_name]} -> #{t[:to_name]}" }
+        raise "No matching transition to #{args.state_name.inspect}. Available:\n" +
+              "  States: #{available_states.join(', ')}\n" +
+              "  Transitions: #{available_transitions.join(', ')}\n" +
+              "  You can use state name, transition name, or transition ID"
       when 1
-        jira "issue/#{ticket_key}/transitions", "transition[id]=%s", [matching.first[:id]], method: :post
+        jira "issue/#{ticket_key}/transitions", "transition[id]=%s", [matching_by_state.first[:id]], method: :post
       else
-        raise "Multiple transitions to #{args.state_name.inspect}: #{matching.map{|t| t[:id]}}"
+        puts "Multiple transitions to #{args.state_name.inspect}:"
+        matching_by_state.each_with_index do |t, i|
+          puts "  #{i + 1}. #{t[:transition_name]} -> #{t[:to_name]} (ID: #{t[:id]})"
+        end
+        puts
+        puts "Please specify the transition by:"
+        puts "  - Transition name: #{matching_by_state.map{|t| t[:transition_name]}.join(' | ')}"
+        puts "  - Transition ID: #{matching_by_state.map{|t| t[:id]}.join(' | ')}"
+        exit 1
       end
     end
 
